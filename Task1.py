86fff8940abe9f472bc01667b46e8f5f29998c15
Алена Киселёва <113586852+KiselevAAl@users.noreply.github.com>
2022-11-28 10:48:58 +0300

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,74 +1,44 @@
 """
-Создайте композицию User состояющую из:
-Объекта Profile со свойствами: name,last_name,age,pasport и методом print_info.
-Объекта Address со свойствами : City,street,zipcode
-Объекта Role со свойствами: role,hours_worked
-Объекта BankAccount со свойствами: card_number, balance
-Объекта Order с методом устанавливающими параметры заказа: Item,date,delivery,price
-Добавьте в композицию методы создания профиля, установки адреса, роли, привязки банковского аккаунта и добавления заказа
+Создайте класс банковского аккаунта по аналогии с примером из презентации. Сделайте атрибут name защищенным, а
+balance и pasport приватными.
+Добавьте геттер-методы на pasport и balance. Сделайте смену номера паспорта по поролю. А изменение баланса
+на определенную сумму(сумма не может падать меньше 0, так что сделайте проверку).
+Создайте метод удаляющий паспортные данные с аккаунта(также по поролю).
 """
 
-class Profile():
-    def __init__(self, name, last_name, age, passport):
-        self.name = name
-        self.last_name = last_name
-        self.age = age
-        self.passport = passport
+class BankAccount():
+    def __init__(self, name, balance, passport):
+        self._name = name
+        self.__balance = balance
+        self.__passport = passport
 
-    def print_info(self):
-        print(self.name, self.last_name, self.passport)
+    def getbalance(self):
+        return self.__balance
 
-class Adress:
+    def getpassport(self):
+        return self.__passport
 
-    def __init__(self,city,street,zipcode):
-        self.city = city
-        self.street = street
-        self.zipcode = zipcode
+    def setbalance(self, new_balance):
+        self.__balance = new_balance
 
+    def setpassport(self, new_passport, password):
+        if password == self.get_password():
+            self.__passport = new_passport
+            print('успешно изменен')
+        else:
+            print('Не тот пароль')
 
-class Role:
-    def __init__(self,role,hours_worked):
-        self.role = role
-        self.hours_worked = hours_worked
-
-
-class BankAccount:
-    def __init__(self,card_number,balance):
-        self.card_number = card_number
-        self.balance = balance
-
-
-class Order:
-    def __init__(self,item,date,delivery,price):
-        self.item = item
-        self.date = date
-        self.delivery = delivery
-        self.price = price
+    def delpassport(self, password):
+        if password == self.getpassword():
+            del self.__passport
+            print('паспорт удален')
+        else:
+            print('Не тот пароль')
 
 
-    def new_order(self):
-        print(self.item,self.date,self.delivery,self.price)
-
-
-class User:
-    def __init__(self):
-        self.adress = []
-        self.bankaccount = []
-        self.order = []
-
-
-    def new_profile(self,name,last_name,passport):
-        self.profile=Profile(name,last_name,passport)
-
-
-    def your_adress(self,city,street,zipcode):
-        self.adress.append(Adress(city,street,zipcode))
-
-
-user1 = User()
-user1.new_profile("Катя","Иванова", 124323)
-user1.your_adress("Сочи","Ленина",354340)
-print(user1.profile)
-print(user1.adress)
-print(user1.profile.name, user1.profile.last_name)
-print(user1.adress[0].city)
+account1 = BankAccount("qwrtp", 1000, 987654)
+print(account1.getbalance())
+print(account1.getpassport())
+account1.setbalance(-34)
+account1.setnomerpassport(9876, 98765)
+account1.delpassport(4567)
